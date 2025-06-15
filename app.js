b666e5dd4135b98c1c944e9d6a14c0d10cf1fe1c
Yarzone51 <yarok92@gmail.com>
2025-06-16 01:43:50 +0300

    2nd commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require('http');
-const hostname = '0.0.0.0'; const port = 3060;
+const hostname = '0.0.0.0'; const port = 3061;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain'); res.end('Hello World');
+  res.setHeader('Content-Type', 'text/plain'); res.end('Hello World 2');
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
